27235351cc02372b87487bdb6096c1a6c7b9c0b4
Wenzhe Zhou <zwzmzd@mail.enjoy-what.com>
2015-12-08 22:04:56 +0800

    支持键盘翻页

--- static/a.js
+++ static/a.js
@@ -1,8 +1,17 @@
 $(document).ready(function() {
 	$("input").on("click",function(){
 		$.post("/giverating", $("#ratingform").serialize(), function(data, status) {
-			console.log(data);
 			$('#tips').removeClass('novisible');
 		});
+	});
+
+	$(document).on('keydown', function(e) {
+		console.log(e.which);
+		if (e.which == 39) {
+			var href = $('li.next').children('a').attr("href");
+			window.location.href = href;
+		} else if (e.which == 37)
+			var href = $('li.previous').children('a').attr("href");
+			window.location.href = href;
 	});
 });
